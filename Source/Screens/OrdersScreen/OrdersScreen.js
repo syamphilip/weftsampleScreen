5ee612a1d9131be59b77f896f89748e53279ca20
Syam Philip <syamphilip96@gmail.com>
2021-08-16 15:51:27 +0530

    CancelOrderModal

--- Source/Screens/OrdersScreen/OrdersScreen.js
+++ Source/Screens/OrdersScreen/OrdersScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,10 +7,12 @@ import {
   StyleSheet,
   Dimensions,
   FlatList,
+  TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
+import Modal from 'react-native-modal';
 
 const heightScreen = Dimensions.get('window').height;
 const widthScreen = Dimensions.get('window').width;
@@ -36,42 +38,49 @@ const ordersData = [
   },
 ];
 
-const orderListView = ({item}) => (
-  <View style={styles.ItemMainView}>
-    <View style={styles.OrderNoAndAEDView}>
-      <Text>Order No: {item.orderNo}</Text>
-      <Text style={styles.AEDText}>{item.AED}</Text>
-    </View>
+function OrdersScreen() {
+  const navigationPointer = useNavigation();
+  const [isModalVisible, setisModalVisible] = useState(false);
 
-    <View style={styles.blueContainer}>
+  const orderListView = ({item}) => (
+    <View style={styles.ItemMainView}>
+      <View style={styles.OrderNoAndAEDView}>
+        <Text>Order No: {item.orderNo}</Text>
+        <Text style={styles.AEDText}>{item.AED}</Text>
+      </View>
+
+      <View style={styles.blueContainer}>
         <View style={styles.OrderPlacedOrderDateView}>
-            <Text>Order</Text>
-            <View style={styles.PlacedView}><Text style={styles.PlacedText}>Placed</Text></View>
-            <View style={styles.OrderDateView}>
-            <IonIcon name="checkmark-circle-outline" size={20} color="gray"/>
+          <Text>Order</Text>
+          <View style={styles.PlacedView}>
+            <Text style={styles.PlacedText}>Placed</Text>
+          </View>
+          <View style={styles.OrderDateView}>
+            <IonIcon name="checkmark-circle-outline" size={20} color="gray" />
             <Text style={styles.OrderDate}>Order Date- {item.OrderDate}</Text>
-            </View>
+          </View>
         </View>
 
         <View style={styles.DeliveryDateView}>
-            <Icon name="truck" size={16} color="gray"/>
-            <Text style={styles.DeliveryDateText}>Delivery Data-{item.DeliveryDate}</Text>
+          <Icon name="truck" size={16} color="gray" />
+          <Text style={styles.DeliveryDateText}>
+            Delivery Data-{item.DeliveryDate}
+          </Text>
         </View>
 
         <View style={styles.ButtonsView}>
-            <TouchableOpacity style={styles.ViewOrderButton}>
-                <Text style={styles.ViewOrderText}>View Order</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.CancelOrderButton}>
+          <TouchableOpacity style={styles.ViewOrderButton}>
+            <Text style={styles.ViewOrderText}>View Order</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.CancelOrderButton}
+            onPress={() => setisModalVisible(true)}>
             <Text style={styles.CancelOrderText}>Cancel Order</Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
+      </View>
     </View>
-  </View>
-);
-
-function OrdersScreen() {
-  const navigationPointer = useNavigation();
+  );
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -100,6 +109,29 @@ function OrdersScreen() {
           showsVerticalScrollIndicator={false}
         />
       </View>
+
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.ModalMainContainer}>
+          <View style={styles.CancelOrderBar}>
+            <Text style={styles.ModalCancelOrderText}>Cancel Order</Text>
+          </View>
+          <View style={styles.TextInputStyleModal}>
+            <TextInput
+              multiline={true}
+              numberOfLines={5}
+              placeholder="Reason for cancel"
+            />
+          </View>
+          <View style={styles.ModalButton}>
+            <TouchableOpacity style={styles.ModalSubmitButton} onPress={()=>setisModalVisible(false)}>
+              <Text style={styles.ModalSubmitText}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.ModalCancelButton} onPress={()=>setisModalVisible(false)}>
+              <Text style={styles.ModalCancelText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -146,7 +178,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 5.0,
     borderTopRightRadius: 5.0,
     borderColor: 'gray',
-    borderWidth: 0.5,
+    borderWidth: 0.2,
     elevation: 3.0,
     backgroundColor: 'white',
     marginBottom: 10.0,
@@ -167,59 +199,109 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 10.0,
   },
-  OrderPlacedOrderDateView:{
-      flexDirection:'row',
-      alignItems:"center"     
+  OrderPlacedOrderDateView: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  PlacedView:{
-      backgroundColor:"#f9c74f",
-      marginLeft:5.0,
+  PlacedView: {
+    backgroundColor: '#f9c74f',
+    marginLeft: 5.0,
   },
-  PlacedText:{
-      color:'white',
-      padding:2.0,
-      paddingHorizontal:5.0,
-      fontWeight:'bold'
+  PlacedText: {
+    color: 'white',
+    padding: 2.0,
+    paddingHorizontal: 5.0,
+    fontWeight: 'bold',
   },
-  OrderDateView:{
-      flexDirection:'row',
-      alignItems:'center',
-      marginLeft:10.0
+  OrderDateView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10.0,
   },
-  OrderDate:{
-      marginLeft:2.0
+  OrderDate: {
+    marginLeft: 2.0,
   },
-  DeliveryDateView:{
-      flexDirection:'row',
-      alignItems:'center',
-      marginVertical:15.0
+  DeliveryDateView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 15.0,
   },
-  DeliveryDateText:{
-      marginLeft:5.0
+  DeliveryDateText: {
+    marginLeft: 5.0,
   },
-  ButtonsView:{
-      flexDirection:'row',
-      alignItems:'center',
-      marginVertical:5.0
+  ButtonsView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5.0,
   },
-  ViewOrderButton:{
-      borderColor:'#43aa8b',
-      borderWidth:1,
-      padding:3.0,
-      paddingHorizontal:10.0,
-      marginRight:10.0
+  ViewOrderButton: {
+    borderColor: '#43aa8b',
+    borderWidth: 1,
+    padding: 3.0,
+    paddingHorizontal: 10.0,
+    marginRight: 10.0,
   },
-  CancelOrderButton:{
-    padding:3.0,
-    paddingHorizontal:10.0,
-    backgroundColor:'#43aa8b'
+  CancelOrderButton: {
+    padding: 3.0,
+    paddingHorizontal: 10.0,
+    backgroundColor: '#43aa8b',
   },
-  ViewOrderText:{
-      fontWeight:'bold',
-      color:'#43aa8b'
+  ViewOrderText: {
+    fontWeight: 'bold',
+    color: '#43aa8b',
   },
-  CancelOrderText:{
-      color:'white',
-      fontWeight:'bold'
+  CancelOrderText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  ModalMainContainer: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+  },
+  CancelOrderBar: {
+    backgroundColor: '#43aa8b',
+    padding: 15.0,
+  },
+  ModalCancelOrderText: {
+    fontSize: 18.0,
+    color: 'white',
+  },
+  TextInputStyleModal: {
+    margin: 15.0,
+    borderWidth: 0.2,
+    borderColor: 'gray',
+    borderRadius: 3.0,
+    padding: 5.0,
+  },
+  ModalButton: {
+    flexDirection: 'row',
+    margin: 15.0,
+    marginBottom: 30.0,
+    alignItems: 'center',
+  },
+  ModalSubmitButton: {
+    backgroundColor: '#43aa8b',
+    padding: 8.0,
+    paddingHorizontal: 15.0,
+    marginRight: 20.0,
+  },
+  ModalSubmitText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 15.0,
+  },
+  ModalCancelButton: {
+    borderWidth: 1,
+    borderColor: '#43aa8b',
+    padding: 8.0,
+    paddingHorizontal: 15.0,
+    marginRight: 20.0,
+  },
+  ModalCancelText:{
+    fontWeight: 'bold',
+    color: '#43aa8b',
+    fontSize: 15.0,
   }
 });
